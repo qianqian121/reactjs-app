e6858a8c5fdfbf01a466b879d60b52cc35056678
qianqian121 <qianqian141@hotmail.com>
2018-06-10 20:29:52 -0700

    initial react server js
    working ok.

---
+++ setup.sh
@@ -0,0 +1,30 @@
+    1  sudo apt-get update
+    2  curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+    3  sudo apt-get install -y nodejs
+   13  #npx create-react-app my-app
+    8  cd ~/project/github/reactjs-app/
+   11  npm start
+   14  sudo add-apt-repository ppa:webupd8team/atom
+   15  sudo apt update; sudo apt install atom
+   16  atom
+
+sudo npm run build
+
+37  cd example/
+   38  ls
+   39  npm install
+   40  ls
+   41  node server.js
+   42  npm install -g browserify
+   43  sudo npm install -g browserify
+   44  node server.js
+   45  sudo npm install -g browserify
+   46  npm ls -g
+   47  sudo npm install browserify --save
+   48  node server.js
+   49  sudo npm install literalify --save
+   50  node server.js
+   51  sudo npm install react-dom-factories --save
+   52  node server.js
+   53  sudo npm install create-react-class --save
+   54  node server.js
